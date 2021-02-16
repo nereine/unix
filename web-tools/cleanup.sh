d788d67248ff53538d47afdfb447a0d2903713cd
artnoi <artdrawin@gmail.com>
2021-02-17 00:03:10 +0700

    Updated web-tools

--- web-tools/cleanup.sh
+++ web-tools/cleanup.sh
@@ -5,11 +5,11 @@
 [[ "$fts_status" != 'ok' ]]\
 && . 'webtools.conf';
 
-## Delete files matching the keywords 'cleanup_find'
-cleanupsh() {
+## Delete files matching the keywords $cleanup_find
+main() {
 	# Get lookup path
 	read -r -p "${0}: Relative path to search (DEFAULT=BLANK=${pwd}): " fdd;
-	# File to delete directory 'fdd' which will contain files to delete 'ftd'	
+	# File to delete directory 'fdd' which will contain files to delete 'ftd', defaults to $(pwd)
 	[ -z "$fdd" ]\
 	&& fdd='.';
 	
@@ -17,21 +17,6 @@ cleanupsh() {
 	# Do NOT double quote the array cleanup_find in the for loop below
 	for k in ${cleanup_find[*]};
 	do
-		findrm() {
-		
-			# if no matched files were found, return to the loop
-			[[ -z "$ftd" ]]\
-			&& printf "%s: No files matching %s\n" "$0" "$k" && return 1\
-			|| printf "%s\n" "$ftd";
-		
-			# if matches were found, prompt user for removal
-			simyn "${0}: Delete these files?"\
-			&& find "$fdd" -path "./.stversions" -prune -false\
-			-o -name "$k" -exec rm -f {} \;
-		
-			unset ynsh;
-		}
-	
 	# cleanup_excl (defined in webtools.conf) is for exclusion
 	# See https://stackoverflow.com/questions/4210042/how-to-exclude-a-directory-in-find-command to understand the line below
 	ftd=$(find "$fdd" -path "$cleanup_excl" -prune -false -o -name "$k"); # files to delete
@@ -40,4 +25,18 @@ cleanupsh() {
 	done;
 }
 
-cleanupsh;
+# Find and remove 'files to delete' $ftd
+findrm() {
+	# if no matched files were found, return to the loop
+	[[ -z "$ftd" ]]\
+	&& printf "%s: No files matching %s\n" "$0" "$k" && return 1\
+	|| printf "%s\n" "$ftd";	
+	# if matches were found, prompt user for removal
+	simyn "${0}: Delete these files?"\
+	&& find "$fdd" -path "./.stversions" -prune -false\
+	-o -name "$k" -exec rm -f {} \;
+
+	unset ynsh;
+}
+
+main;
