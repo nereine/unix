2b17a6673d774e17ecf1374d9d675e92d6288f2a
artnoi <artdrawin@gmail.com>
2021-02-16 19:35:58 +0700

    Changed swaylock config

--- sh-tools/bin/cron/logtrim.sh
+++ sh-tools/bin/cron/logtrim.sh
@@ -3,7 +3,7 @@ Xorg_log=("/home/artnoi/.local/share/xorg"/*)
 otherlog=("/var/log/Xorg.0.log" "/home/artnoi/.cache/openbox/openbox.log")
 cleanlog=(${Xorg_log[@]} "${otherlog[@]}")
 # printf "%s\n" "Trimming the following log files:" "${cleanlog[@]}"
-for logfile in "${cleanlog[@]}"; do
+for logfile in "${cleanlog[@]}";
+do
 	tail -n 50 "$logfile" | sudo tee "$logfile" >/dev/null 2>&1 ;
-done
-exit
+done;
