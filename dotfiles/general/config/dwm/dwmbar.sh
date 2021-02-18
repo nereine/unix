c75d2f1976a568cd0c83adc652f10cf64f8c4dcb
artnoi <artdrawin@gmail.com>
2021-02-18 20:12:16 +0700

    Updated up and dwmbar.sh

--- dotfiles/general/config/dwm/dwmbar.sh
+++ dotfiles/general/config/dwm/dwmbar.sh
@@ -1,34 +1,38 @@
+#!/bin/sh
 # dwmbar.sh - dwm status bar script using 'xsetroot -name'
 
-## Get static status information
-# These 2 items should work on most systems
-myname="$(whoami)";
-myhost="$HOSTNAME";
+## Get static status information - These lines should work, yet it doesn't on my Sway setup except the one using 'whoami'
+#myname="$(whoami)";
+#myname="$USER"
+#myhost=$(< /etc/hostname);
+#myhost="$HOSTNAME"
+
 # Override example
-myhost="t14"
+myname="artnoi";
+myhost="t14";
 
 ## Get dynamic information
 get_status() {
-	sdata="$(sensors|grep -A3 thinkpad)"
-	temp="$(echo $sdata | awk '{print $9}')";
-	fans="$(echo $sdata | awk '{print $6}')";
-	batstat="$(acpi|awk '{print $4}'|sed 's/,//')";
-	# Time format = DoW Hour:Min
-	loctime="$(date +'%A %b %d | %H:%M')";
+	data="$(sensors|grep -A3 thinkpad)";
+	temp="$(echo $data | awk '{print substr($9,2); }')";
+	fans="$(echo $data | awk '{print $6 " rpm"}')";
+	batt="$(acpi -b | awk '{print tolower(substr($3, 1, length($3)-1)) ": " substr($4, 1, length($4)-1)}';)"; # Battery format = percentage%, status(charge/discharge)
+	time="$(date +'%A, %b %d - %H:%M' | awk '{print tolower($0)}';)"; # Time format = DoW Hour:Min
 
-	printf "temp: %s rpm | batt: %s | %s | %s" "$temp/$fans" "$batstat" "$loctime" "$myname@$myhost";
+	printf "%s | %s | %s | %s" "${myname}@${myhost}" "${temp}/${fans}" "$batt" "$time";
 }
 
 main() {
-	while true; do
+	while true;
+	do
 		# $WAYLAND is exported from $HOME/{.kshrc, .bash_profile
 		[ -z $WAYLAND ]\
 		&& xsetroot -name "$(get_status)"\
 		&& arg='&'\
 		|| echo "$(get_status)";
 		# Status update interval
-		sleep 5;
+		sleep 2;
 	done;
 }
 
-main $arg
+main "$arg";
