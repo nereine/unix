3c328673dced09995e39c845f0c415f44cacacd1
artnoi <artdrawin@gmail.com>
2021-02-22 18:58:17 +0700

    Updated dembar.sh

--- dotfiles/general/config/dwm/dwmbar.sh
+++ dotfiles/general/config/dwm/dwmbar.sh
@@ -10,18 +10,25 @@ myhost="t14";
 
 ## Get dynamic information - I tried my best to optimize this because it may be run very frequently
 get_status() {
-	
-	time="$(date +'%A, %b %d > %H:%M' | awk '{print tolower($0)}';)";
-	batt=$(acpi -b | awk '{print tolower(substr($3, 1, length($3)-1)) ": " substr($4, 1, length($4)-1)}';);
-	thermals=$(sensors|awk '/fan/{c=2}c&&c-- {printf "%s ",$2} END {print ""}');
+# Battery information is retrieved by 'acpi -b' and later formatted with awk to show the charging state and charge percentage
+# Thermal information is retrieved by 'lm_sensors' and later formatted with awk. awk searches for desired string with regexp, in the default case it is /fan/, although you can always use other regxep to retrieve different lines from lm_sensors ('sensors')
 
-	printf "%s | %s | %s | %s" "${myname}@${myhost}" "RPM $thermals" "$batt" "$time";
+	time="$(date +'%A, %b %d > %H:%M'\
+	| awk '{print tolower($0)}';)";
+	batt=$(acpi -b\
+	| awk '{print tolower(substr($3, 1, length($3)-1)) ": " substr($4, 1, length($4)-1)}';);
+	thermals=$(sensors\
+	| awk '/fan/{c=2}c&&c-- {printf "%s ",$2} END {print ""}');
+
+	# $thermals has trailing space, so the printf format below is a bit funny. Note that ${str%???} will remove 3 last chars from $str
+	printf "%s | %s | %s | %s\n" "${myname}@${myhost}" "RPM ${thermals%?}" "$batt" "$time";
 }
 
 main() {
 	while true;
 	do
-		# $WAYLAND is exported from $HOME/{.kshrc, .bash_profile
+		## $WAYLAND is exported from $HOME/{.kshrc, .bash_profile}
+		# $arg is used to idiotically enable forking on dwm, and therefore is needed for main()
 		[ -z $WAYLAND ]\
 		&& xsetroot -name "$(get_status)"\
 		&& arg='&'\
@@ -31,7 +38,9 @@ main() {
 	done;
 }
 
+# For testing, comment out main() and uncomment get_status()
 main "$arg";
+#get_status;
 	
 ### other ways to get data
 ## Get static status information - These lines should work, yet it doesn't on my Sway setup except the one using 'whoami'
